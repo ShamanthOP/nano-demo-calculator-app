68b025762c2a672ef12b4d6974b4d3a9d15f1858
Shamanth <shamanth1604@gmail.com>
2023-05-28 12:03:28 +0530

    testing workflow

--- server.js
+++ server.js
@@ -1,25 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-const baseUrl = '/calculator'
+const baseUrl = "/calculator";
 
 app.use(express.json());
 
 const baseRouter = express.Router();
 
-baseRouter.get('/greeting', (req, res) => {
-    return res.send('');
+baseRouter.get("/greeting", (req, res) => {
+    return res.send("Hello World!");
 });
 
-baseRouter.post('/add', (req, res) => {
-    res.json({ "": null });
+baseRouter.post("/add", (req, res) => {
+    const { first, second } = req.body;
+    const res = first + second;
+    res.status(200).json({ result: res });
 });
 
-
-baseRouter.post('/subtract', (req, res) => {
-    res.json({ "": null });
+baseRouter.post("/subtract", (req, res) => {
+    const { first, second } = req.body;
+    const res = first - second;
+    res.status(200).json({ result: res });
 });
 
 app.use(baseUrl, baseRouter);
